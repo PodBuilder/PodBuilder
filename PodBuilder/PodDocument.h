1615f59f05b05d9ffe23d99e4f340a7bed63a81b
William Kent <wjk011@gmail.com>
2014-04-04 15:33:55 -0400

    Initial Commit

---
+++ PodBuilder/PodDocument.h
@@ -0,0 +1,13 @@
+//
+//  PodDocument.h
+//  PodBuilder
+//
+//  Created by William Kent on 4/4/14.
+//  Copyright (c) 2014 William Kent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PodDocument : NSDocument
+
+@end
---
+++ PodBuilder/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PodBuilder
+//
+//  Created by William Kent on 4/4/14.
+//  Copyright (c) 2014 William Kent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
